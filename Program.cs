8a6fde46cd9e220713481649211e77f88652f430
melike tutkun <meliketutkun2422@gmail.com>
2022-08-10 12:01:16 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+//integer tanımlama şekli
+int integer1=5;
+int integer2=3;
+int integer3=integer1+integer2;
+Console.WriteLine(integer3);
+
+//bool tanımlama şekli
+bool bool1=10>100;
+Console.WriteLine(bool1);
+
+//string tanımlama şekli
+string str20="20";
+int int20=20;
+string yenideger=str20+int20.ToString();
+Console.WriteLine(yenideger);
+
+int int21=int20+Convert.ToInt32(str20);
+Console.WriteLine(int21);
